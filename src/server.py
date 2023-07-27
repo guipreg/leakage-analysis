367746455a0699a4a1240af3cd0e7d62c8296d9b
michel.sialer-cruz.1@ens.etsmtl.ca <michel.sialer-cruz.1@ens.etsmtl.ca>
2023-07-27 14:09:40 -0400

    removing upload endpoint, merging its code with that of analyze endpoint and updating swagger

--- src/server.py
+++ src/server.py
@@ -1,9 +1,9 @@
 import sys, os
 from yaml import Loader, load
 from flask import Flask, request, Response
-from .main import main
 from werkzeug.utils import secure_filename
 from flask_swagger_ui import get_swaggerui_blueprint
+from .main import main
 
 UPLOAD_FOLDER = "/var/www/uploads"
 ALLOWED_EXTENSIONS = {"ipynb"}
@@ -13,7 +13,7 @@ API_URL = "./static/swagger.yml"
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-swagger_yml = load(open(API_URL, "r"), Loader=Loader)
+swagger_yml = load(open(API_URL, "r", encoding="utf-8"), Loader=Loader)
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
@@ -25,37 +25,52 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 app.register_blueprint(swaggerui_blueprint)
 
 
+class APIError(Exception):
+    def __init__(self, message, code):
+        self.message = message
+        self.code = code
+
+
+@app.errorhandler(APIError)
+def default_error_handler(e):
+    return {"message": e.message}, e.code
+
+
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.post("/upload")
-def upload():
-    file = request.files["file"]
-    if not file:
-        return "No file sent", 400
+@app.post("/analyze")
+def analyze():
+    filename = upload_file(request)
+    return analyze_file(filename)
+
+
+def upload_file(req):
+    if "file" not in req.files:
+        raise APIError("No file sent", 400)
+    file = req.files["file"]
     if not allowed_file(file.filename):
-        return "File not allowed", 400
-    filename = secure_filename(file.filename)
+        raise APIError("File not allowed", 400)
     # TODO: save in parent folder ?
-    file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    filename = secure_filename(file.filename)
+    filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+    file.save(filepath)
+    if not os.path.isfile(filepath):
+        raise APIError("File could not be uploaded", 500)
     return filename
 
 
-@app.get("/analyze/<filename>")
-def analyze(filename):
+def analyze_file(filename):
     filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-    if not os.path.isfile(filepath):
-        return "File not found", 404
     sys.argv = ["src.main", filepath]
     try:
         main(filepath)
     except Exception as e:
-        return "Error occured during analysis", 500
-    htmlfilepath = os.path.join(
+        raise APIError("Error occured during analysis", 500) from e
+    jsonpath = os.path.join(
         app.config["UPLOAD_FOLDER"], f"{filename[:-6]}_results.json"
     )
-    if os.path.isfile(htmlfilepath):
-        return Response(open(htmlfilepath, "r"), mimetype="application/json")
-    else:
-        return "No report file produced", 500
+    if not os.path.isfile(jsonpath):
+        raise APIError("No result was produced from analysis", 500)
+    return Response(open(jsonpath, "r", encoding="utf-8"), mimetype="application/json")
